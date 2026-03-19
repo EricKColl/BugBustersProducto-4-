df7d73dbcb5fd04c4729ec59954d344bb9ad9614
Erick Coll Rodríguez <erickcollrodriguez@gmail.com>
2026-03-19 20:47:24 +0100

    Retoque final al Skeleton

--- src/Database/producto3.sql
+++ src/Database/producto3.sql
@@ -1,49 +1,50 @@
 --Este archivo lo requiere la actividad y lo tenemos por si pasara algo--
 
--- =========================================
--- BASE DE DATOS
--- =========================================
-CREATE DATABASE IF NOT EXISTS producto3
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-USE producto3;
-
--- =========================================
+-- =========================
 -- TABLA CLIENTES
--- =========================================
+-- =========================
 CREATE TABLE clientes (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
-    nombre VARCHAR(100),
+    nombre VARCHAR(100) NOT NULL,
     domicilio VARCHAR(150),
     nif VARCHAR(20),
-    tipo_cliente VARCHAR(20) -- ESTANDAR / PREMIUM
+    tipo_cliente ENUM('estandar', 'premium') NOT NULL
 );
 
--- =========================================
+-- =========================
 -- TABLA ARTICULOS
--- =========================================
+-- =========================
 CREATE TABLE articulos (
     id_articulo INT AUTO_INCREMENT PRIMARY KEY,
     codigo VARCHAR(50) NOT NULL UNIQUE,
-    descripcion VARCHAR(150),
-    precio_venta DOUBLE,
-    gastos_envio DOUBLE,
+    descripcion VARCHAR(200) NOT NULL,
+    precio_venta DECIMAL(10,2) NOT NULL,
+    gastos_envio DECIMAL(10,2),
     tiempo_preparacion INT
 );
 
--- =========================================
+-- =========================
 -- TABLA PEDIDOS
--- =========================================
+-- =========================
 CREATE TABLE pedidos (
     id_pedido INT AUTO_INCREMENT PRIMARY KEY,
-    id_cliente INT,
-    id_articulo INT,
-    cantidad INT,
-    fecha_hora DATETIME,
-    estado VARCHAR(20) DEFAULT 'PENDIENTE',
 
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
-    FOREIGN KEY (id_articulo) REFERENCES articulos(id_articulo)
+    id_cliente INT NOT NULL,
+    id_articulo INT NOT NULL,
+
+    cantidad INT NOT NULL,
+    fecha_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
+    estado VARCHAR(50),
+
+    -- RELACIONES
+    CONSTRAINT fk_cliente
+        FOREIGN KEY (id_cliente)
+        REFERENCES clientes(id_cliente)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_articulo
+        FOREIGN KEY (id_articulo)
+        REFERENCES articulos(id_articulo)
+        ON DELETE CASCADE
 );
