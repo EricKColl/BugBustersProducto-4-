ef72b313310fb3b72aee13354b83ca768c7023b3
Ferranarna <ferran.arnaus.garcia23@gmail.com>
2026-03-24 23:36:48 +0100

    Add Producto 2 files, refactor ConexionBD to use DAOExceptions

--- src/Excepciones/DAOException.java
+++ src/Excepciones/DAOException.java
@@ -1,7 +1,9 @@
 package Excepciones;
 
+import java.sql.SQLException;
+
 public class DAOException extends Exception {
-    public DAOException(String mensaje) {
+    public DAOException(String mensaje, SQLException e) {
         super(mensaje);
     }
 }
--- src/Util/ConexionBD.java
+++ src/Util/ConexionBD.java
@@ -1,5 +1,7 @@
 package Util;
 
+import Excepciones.DAOException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,6 +12,29 @@ public class ConexionBD {
     private static final String USER = "root";
     private static final String PASSWORD = "SppuTCrhvoNHXhezDpJcwTINkOenYool";
 
+    // Objeto que mantiene la sesión abierta
+    private Connection conexion;
+
+    // Método para abrir la conexión
+    public void conectar() throws DAOException {
+        try {
+            // DriverManager es una clase de Java que busca el driver de MySQL y abre la puerta
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new DAOException("Error grave: No se pudo conectar a la base de datos MySQL.", e);
+        }
+    }
+
+    // Método para cerrar la conexión
+    public void desconectar() throws DAOException {
+        try {
+            if (conexion != null && !conexion.isClosed()) {
+                conexion.close();
+            }
+        } catch (SQLException e) {
+            throw new DAOException("Error al intentar cerrar la conexión con la base de datos.", e);
+        }
+    }
     public static Connection getConexion() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
